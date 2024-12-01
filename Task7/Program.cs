97e9e0fea53606564463ec89ab99c660220c1385
Aishah999 <aishaah@code.edu.az>
2024-12-01 11:47:13 +0400

    add commit

---
+++ Task7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace ClassLibrary;
+public class Helper
+{
+    public string CreateBookCode(string bookName,int number)
+    {
+        return bookName.Substring(0, 2).ToUpper() + number;
+    } 
+}
+public class Book
+{
+    public string Name { get; set; }
+    public string AuthorName { get; set; }
+    public int PageCount { get; set; }
+    public string Code { get; set; }
+
+    public Book(string name,string authorName,int pageCount,int number)
+    {
+        Name = name;AuthorName = authorName;
+        PageCount = pageCount;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        ClassLibrary.Book book = new ClassLibrary.Book("Programming", "John Doe", 300, 10);
+        Console.WriteLine("Kitab adi:" + book.Name);
+        Console.WriteLine("Muellif:" + book.AuthorName);
+        Console.WriteLine("Sehife sayi:" + book.PageCount);
+        Console.WriteLine("Kitab kodu:" + book.Code);
+    }
+}
